d870528ed93213ee5d67adf81be14bfeeb68e91c
Chris Stone <ircstones@gmail.com>
2022-04-29 00:43:06 +0000

    Point backend to correct index message

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Other Worldly" > index.html
+              echo "Hello, Other Worldly One" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
